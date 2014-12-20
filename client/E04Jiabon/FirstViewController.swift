2e352c1ca87895653f556105bb817f4e58928431
iNickGo <inickgo@gmail.com>
2014-12-21 07:37:32 +0800

    revise client usage

--- client/E04Jiabon/FirstViewController.swift
+++ client/E04Jiabon/FirstViewController.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 
+import MobileCoreServices
 
-class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
-    let client : Client = Client()
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,UIImagePickerControllerDelegate,UINavigationControllerDelegate  {
+    
+    var cameraUI: UIImagePickerController! = UIImagePickerController()
+    
+    let client : Client = Client.sharedInstance
     let APP_MESSAGE : String = "E04 甲奔拉！"
     
     @IBOutlet weak var myTabelView: UITableView!
@@ -28,11 +32,82 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
         myTabelView.delegate = self
         myTabelView.dataSource = self
         
+
         client.initLocation()
         client.setInfo("nick", pwd: "1234")
         client.connect()
+        
+    }
+    
+    func presentCamera()
+    {
+
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        {
+            cameraUI = UIImagePickerController()
+            cameraUI.delegate = self
+            cameraUI.sourceType = UIImagePickerControllerSourceType.Camera;
+            cameraUI.mediaTypes = [kUTTypeImage]
+            cameraUI.allowsEditing = false
+            cameraUI.cameraDevice = UIImagePickerControllerCameraDevice.Front
+            
+            self.presentViewController(cameraUI, animated: true, completion: nil)
+        }
+        else
+        {
+            // error msg
+        }
     }
 
+    func imagePickerControllerDidCancel(picker:UIImagePickerController)
+    {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    
+    func imagePickerController(picker:UIImagePickerController!, didFinishPickingMediaWithInfo info:NSDictionary)
+    {
+        if(picker.sourceType == UIImagePickerControllerSourceType.Camera)
+        {
+            var image: UIImage = info.objectForKey(UIImagePickerControllerOriginalImage) as UIImage
+            var newImg = RBResizeImage(image, targetSize: CGSizeMake(200, 100))
+            var data  = UIImageJPEGRepresentation(newImg, 50)
+            
+            let base64String = data.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.allZeros)
+            
+            client.uploadImg(base64String)
+
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        
+    }
+    
+    func RBResizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / image.size.width
+        let heightRatio = targetSize.height / image.size.height
+        
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSizeMake(size.width * heightRatio, size.height * heightRatio)
+        } else {
+            newSize = CGSizeMake(size.width * widthRatio,  size.height * widthRatio)
+        }
+        
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRectMake(0, 0, newSize.width, newSize.height)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.drawInRect(rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -62,6 +137,8 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
 
         showLocalNotify(name)
         showAlertMsg(name)
+
+       // self.presentCamera()
     }
     
     func showAlertMsg(name: String)
