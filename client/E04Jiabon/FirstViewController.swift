eba610e3aac8eb44e003f7bc495a38905e21a670
Sandra167 <sandra@sandradeMacBook-Pro.local>
2014-12-21 08:02:04 +0800

    revise UI

--- client/E04Jiabon/FirstViewController.swift
+++ client/E04Jiabon/FirstViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 import MobileCoreServices
 
+struct TOnlineUser
+{
+    var name: String
+    var distance = 0
+}
+
 class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,UIImagePickerControllerDelegate,UINavigationControllerDelegate  {
     
     var cameraUI: UIImagePickerController! = UIImagePickerController()
@@ -19,13 +25,20 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     @IBOutlet weak var myTabelView: UITableView!
     
+    // this is fake data
     var ListArray = ["Sandra", "Steven", "Jerry", "Nick", "Diro", "Bin", "Jack", "Ives", "Emma", "Rick", "阿毛", "蕃薯", "兩百", "Trista", "Joyce", "John"]
+   
+    var userList: [TOnlineUser] = []
     
     var alert: UIAlertView = UIAlertView()
+    
+    var userDefault = NSUserDefaults.standardUserDefaults()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        searchDataBase()
         
         // register tabel view
         self.myTabelView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
@@ -36,6 +49,14 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
         client.initLocation()
         client.setInfo("nick", pwd: "1234")
         client.connect()
+        
+        var storedNumber = userDefault.integerForKey("number")
+        //userDefault.setInteger(number , forKey: "number")
+        //userDefault.synchronize()
+        println("read:" + toString(storedNumber))
+        storedNumber+=1
+        userDefault.setInteger(storedNumber , forKey: "number")
+        userDefault.synchronize()
         
     }
     
@@ -114,31 +135,36 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
-        println("ListCount = " + toString(self.ListArray.count));
-        return self.ListArray.count
+        println("ListCount = " + toString(self.userList.count));
+        return self.userList.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     
+        let cellIdentifier = "cell"
         var cell: UITableViewCell = self.myTabelView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
-        println(toString(indexPath.row) + " : " + self.ListArray[indexPath.row]);
-        cell.textLabel.text = self.ListArray[indexPath.row] + "\t" + toString(indexPath.row)
+        println(toString(indexPath.row) + " : " + self.userList[indexPath.row].name);
+        
+        cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: cellIdentifier)
+        cell.textLabel?.text = self.userList[indexPath.row].name
+        cell.detailTextLabel?.text = toString(self.userList[indexPath.row].distance) + " 公尺"
+        
         return cell
     }
     
     func tableView(tableView: UITableView, accessoryButtonTappedForRowWithIndexPath indexPath: NSIndexPath) {
-        println("accessoryButtonTappedForRowWithIndexPath : " + toString(indexPath.row) + " : " + self.ListArray[indexPath.row]);
+        println("accessoryButtonTappedForRowWithIndexPath : " + toString(indexPath.row) + " : " + self.userList[indexPath.row].name);
     }
     
     func tableView(tableView: UITableView, didHighlightRowAtIndexPath indexPath: NSIndexPath) {
-        var name: String = self.ListArray[indexPath.row]
+        var name: String = self.userList[indexPath.row].name
         
-        println("didHighlightRowAtIndexPath : " + toString(indexPath.row) + " : " + self.ListArray[indexPath.row]);
+        println("didHighlightRowAtIndexPath : " + toString(indexPath.row) + " : " + self.userList[indexPath.row].name);
 
-        showLocalNotify(name)
-        showAlertMsg(name)
+        sendNameMsgTo(name)
+        recvMsgFrom(name)
 
-       // self.presentCamera()
+         self.presentCamera()
     }
     
     func showAlertMsg(name: String)
@@ -157,9 +183,40 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
         UIApplication.sharedApplication().scheduleLocalNotification(notification)
     }
     
-    func sendNameMsg()
+    func searchDataBase()
     {
-        //
+        // TO DO: server give ui a list of name and distance
+        // please convert data to struct TOnlineUser
+        // then append data to userList
+        
+        
+        // this is a sample
+        for index in 0...(ListArray.count - 1) {
+            var data: TOnlineUser = TOnlineUser(name: ListArray[index], distance: index * 10)
+            self.userList.append(data)
+        }
+    }
+    
+    func sendNameMsgTo(name: String)
+    {
+        // TO DO: Server send Message to name
+        println("sendNameMsgTo : " + name);
+        
+        
+        
+    }
+    
+    func recvMsgFrom(name: String)
+    {
+        // TO DO: Server receive Message to name
+        println("recvMsgFrom : " + name);
+        
+        
+        
+        
+        // UI show Notify and alert window
+        showLocalNotify(name)
+        showAlertMsg(name)
     }
 }
 
