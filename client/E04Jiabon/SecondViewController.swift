417cba82d455eedd65e7d42d54594eb5e7575eb3
iNickGo <inickgo@gmail.com>
2014-12-21 13:18:37 +0800

    team page

--- client/E04Jiabon/SecondViewController.swift
+++ client/E04Jiabon/SecondViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBAction func onClick(sender: AnyObject) {
+        println("test") 
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
